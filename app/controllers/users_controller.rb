d2459c51042d7a1a5a41ca532fca3ad78ed03061
Sarah Schaller <sarcsch@aol.com>
2018-04-30 22:24:28 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
-class UsersController < ApplicationController 
-  def new 
-  end 
+class UsersController < ApplicationController
+  def new
+  end
 
-  def create 
-    @user = User.new 
+  def create
+    @user = User.new
   end
 end
